46fa2fbf60ac68290fc4098f58ae5340fb8d3c6c
Kyungwoo Ko <eyeofthei21@gmail.com>
2022-10-02 12:42:44 +0900

    Add root path route handler

--- app.js
+++ app.js
@@ -57,6 +57,6 @@ app.delete("/api/members/:id", async (req, res) => {
   }
 });
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(3000, () => {
   console.log("Server is listening...");
 });
